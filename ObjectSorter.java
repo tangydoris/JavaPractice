0327a3b3c4d673e76cdf0f9d031a37c9f9c39679
Doris Tang <huiyao.tang@nyu.edu>
2015-07-16 13:02:49 -0400

    * Created ObjectSorter

---
+++ ObjectSorter.java
@@ -0,0 +1,19 @@
+
+public class ObjectSorter {
+	
+	/*
+	 * Given `N' objects Numbered 0, 1 or 2, sort them so that objects of the same 
+	 * numbers are adjacent, with the numbers in the sorted order.
+	 * For e.g.
+	 * input
+	 * 1,2,0,1,2,2,1,0
+	 * output
+	 * 0,0,1,1,1,2,2,2
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

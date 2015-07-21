c9f1dcc4e9bc95c71ff0197b9b9a738c0eff8ec8
Doris Tang <huiyao.tang@nyu.edu>
2015-07-21 10:41:24 -0400

    * Implemented LargestProductFinder

--- LargestProductFinder.java
+++ LargestProductFinder.java
@@ -1,3 +1,4 @@
+import java.util.logging.Logger;
 
 public class LargestProductFinder {
 	
@@ -27,16 +28,52 @@ public class LargestProductFinder {
 	
 	/*
 	 * Method two:
-	 * Look at all subarrays that contain only positive integers
-	 * Imagine a new array that contains the products of those subarrays
-	 * and the negatives left over (now we have an array with alternating
-	 * positives and negatives)
-	 * 
+	 * Keep track of the current product, global maximum product,
+	 * 	current maximum product, current minimum product
+	 * With this we do not have to keep track of the number of negatives
+	 * 	or worry about signs
+	 * CODE THIS
 	 */
+	
+	static Logger logger = Logger.getLogger("LargestProductFinder.class");
+	
+	public static int findLargestProduct(int[] a) {
+		// This null case handling depends on what the application calls for
+		if (a == null) {
+			logger.severe("Input cannot be null.");
+			throw new RuntimeException("Input cannot be null.");
+		}
+		// Also depends on application specs
+		if (a.length == 0) {
+			logger.severe("Input array cannot be empty");
+			throw new RuntimeException("Input array cannot be empty");
+		}
+		
+		int currMax, currMin, globalMax, prevMax, prevMin;
+		currMax = currMin = globalMax = prevMax = prevMin = 1;
+		for (int i = 0; i < a.length; i++) {
+			if (((prevMax > 0 && a[i] > 0) || (prevMax < 0 && a[i] < 0)) & prevMax*a[i] < 0) {
+				logger.severe("Maximum integer supported reached.");
+				throw new LargeNumberException("Maximum integer supported reached.");
+			}
+			if (((prevMax > 0 && a[i] < 0) || (prevMax < 0 && a[i] > 0)) & prevMax*a[i] > 0) {
+				logger.severe("Minimum integer supported reached.");
+				throw new LargeNumberException("Minimum integer supported reached.");
+			}
+				
+			currMax = Math.max(Math.max(a[i], prevMax*a[i]), prevMin*a[i]);
+			currMin = Math.min(Math.min(a[i], prevMax*a[i]), prevMin*a[i]);
+			globalMax = Math.max(globalMax, currMax);
+			prevMax = currMax;
+			prevMin = currMin;
+		}
+		System.out.println(globalMax);
+		return globalMax;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] a = {1,0,-7,-4,0,4,5,-2,3,1,5};
+		findLargestProduct(a);
 	}
 
 }
